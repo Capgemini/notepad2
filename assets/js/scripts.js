c46722f3284469d4d5bc9135850f7090f2ef5ba3
Malcolm Young <malcolm.young@capgemini.com>
2015-10-12 11:37:23 +0100

    initial commit of CSS and JS

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+---
+---
+
+{% include_relative vendor/modernizr.js %}
+{% include_relative foundation.min.js %}
+
+{% include_relative notepad.js %}
+{% include_relative scripts.min.js %}
+{% include_relative vendor/nprogress.js %}
+{% include_relative search.js %}
+{% include_relative vendor/footnote-links.js %}
