05c884fb922efcd8e310b431f247067af55d51ca
Malcolm Young <malcolm.young@capgemini.com>
2015-10-21 06:52:21 +0200

    put notepad at end of scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@
 {% include_relative vendor/modernizr.js %}
 {% include_relative foundation.min.js %}
 
-{% include_relative notepad.js %}
 {% include_relative scripts.min.js %}
 {% include_relative search.js %}
 {% include_relative vendor/footnote-links.js %}
+{% include_relative notepad.js %}
