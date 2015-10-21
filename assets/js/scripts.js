83f4bb86b8206e0e44b135f624d3567c25220d11
Malcolm Young <malcolm.young@capgemini.com>
2015-10-21 06:02:17 +0200

    remove NProgress

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,5 @@
 
 {% include_relative notepad.js %}
 {% include_relative scripts.min.js %}
-{% include_relative vendor/nprogress.js %}
 {% include_relative search.js %}
 {% include_relative vendor/footnote-links.js %}
